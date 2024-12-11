abc003a6b14ce41358d6dd73c01af1cef2ae0ce9
rochadasilvamatheus <mathewsr.silva@gmail.com>
2024-12-11 14:35:33 -0300

    feat: testes de order

---
+++ src/entity/order.spec.ts
@@ -0,0 +1,36 @@
+import Order from "./order";
+import OrderItem from "./order_item";
+
+describe("Order unit tests", () => {
+  it("should throw error when id is empty", () => {
+    expect(() => {
+      let order = new Order("", "123", []);
+    }).toThrow("Id is required");
+  });
+
+  it("should throw error when customerId is empty", () => {
+    expect(() => {
+      let order = new Order("123", "", []);
+    }).toThrow("CustomerId is required");
+  });
+
+  it("should throw error when items is empty", () => {
+    expect(() => {
+      let order = new Order("123", "123", []);
+    }).toThrow("Items are required");
+  });
+
+  it("should calculate total", () => {
+    const item = new OrderItem("i1", "item 1", 100);
+    const item2 = new OrderItem("i2", "item 2", 200);
+    const order = new Order("o1", "c1", [item]);
+
+    let total = order.total();
+
+    expect(total).toBe(100);
+
+    const order2 = new Order("o1", "c1", [item, item2]);
+    total = order2.total();
+    expect(total).toBe(300);
+  });
+});
