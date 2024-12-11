1f05de81fb5742c00a2664daccf0c53ea0892b2b
rochadasilvamatheus <mathewsr.silva@gmail.com>
2024-12-11 11:57:46 -0300

    feat: adicionado entity e objetos de valor

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,31 +1,43 @@
-class Customer {
+import Address from "./address";
+
+export default class Customer {
   _id: string;
   _name: string;
-  _address: string;
+  _address!: Address;
+  _active: boolean = false;
 
-  constructor(id: string, name: string, address: string) {
+  constructor(id: string, name: string) {
     this._id = id;
     this._name = name;
-    this._address = address;
+    this.validate();
   }
 
-  get id(): string {
-    return this._id;
+  validate() {
+    if (this._name.length === 0) {
+      throw new Error("Name is required");
+    }
+    if (this._id.length === 0) {
+      throw new Error("Id is required");
+    }
   }
 
-  get name(): string {
-    return this._name;
-  }
-
-  get address(): string {
-    return this._address;
-  }
-
-  set name(name: string) {
+  changename(name: string) {
     this._name = name;
+    this.validate();
   }
 
-  set address(address: string) {
+  activate() {
+    if (this._address === undefined) {
+      throw new Error("Address is mandatory to activate a customer");
+    }
+    this._active = true;
+  }
+
+  deactivate() {
+    this._active = false;
+  }
+
+  set Address(address: Address) {
     this._address = address;
   }
 }
