667907bb09f066a2a0594cb6cdc76cf4933564b0
rochadasilvamatheus <mathewsr.silva@gmail.com>
2024-12-12 18:28:34 -0300

    feat: adicionado product repository create

--- src/infrastructure/repository/product.repository.spec.ts
+++ src/infrastructure/repository/product.repository.spec.ts
@@ -1,4 +1,7 @@
 import { Sequelize } from "sequelize-typescript";
+import ProductModel from "../db/sequelize/model/product.model";
+import Product from "../../domain/entity/product";
+import ProductRepository from "./product.repository";
 
 describe("Product repository test", () => {
   let sequelize: Sequelize;
@@ -11,8 +14,26 @@ describe("Product repository test", () => {
       sync: { force: true }, // Synchronize and create tables
     });
 
-    afterEach(async () => {
-      await sequelize.close();
+    sequelize.addModels([ProductModel]);
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+
+  it("should create a product", async () => {
+    const productRepository = new ProductRepository();
+    const product = new Product("1", "Product 1", 100);
+
+    await productRepository.create(product);
+
+    const productModel = await ProductModel.findOne({ where: { id: "1" } });
+
+    expect(productModel.toJSON()).toStrictEqual({
+      id: "1",
+      name: "Product 1",
+      price: 100,
     });
-  }).before;
+  });
 });
