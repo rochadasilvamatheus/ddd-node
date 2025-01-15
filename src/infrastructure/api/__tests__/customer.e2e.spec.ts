6f7b3c7bf4ebac49fa0c980fc76036900b2763fa
rochadasilvamatheus <mathewsr.silva@gmail.com>
2025-01-15 16:26:50 -0300

    feat: adicionado e configurado express e rota para consumer

---
+++ src/infrastructure/api/__tests__/customer.e2e.spec.ts
@@ -0,0 +1,80 @@
+import { app, sequelize } from "../express";
+import request from "supertest";
+
+describe("E2E test for customer", () => {
+  beforeEach(async () => {
+    await sequelize.sync({ force: true });
+  });
+
+  afterAll(async () => {
+    await sequelize.close();
+  });
+
+  it("should create a customer", async () => {
+    const response = await request(app)
+      .post("/customer")
+      .send({
+        name: "John Doe",
+        address: {
+          street: "Main Street",
+          city: "Springfield",
+          number: 123,
+          zip: "12345",
+        },
+      });
+
+    expect(response.status).toBe(200);
+    expect(response.body.name).toBe("John Doe");
+    expect(response.body.address.street).toBe("Main Street");
+    expect(response.body.address.city).toBe("Springfield");
+    expect(response.body.address.number).toBe(123);
+    expect(response.body.address.zip).toBe("12345");
+  });
+
+  it("should not create a customer", async () => {
+    const response = await request(app).post("/customer").send({
+      name: "john",
+    });
+    expect(response.status).toBe(500);
+  });
+
+  it("should list all customers", async () => {
+    const response = await request(app)
+      .post("/customer")
+      .send({
+        name: "John Doe",
+        address: {
+          street: "Main Street",
+          city: "Springfield",
+          number: 123,
+          zip: "12345",
+        },
+      });
+
+    expect(response.status).toBe(200);
+
+    const response2 = await request(app)
+      .post("/customer")
+      .send({
+        name: "Jane Doe",
+        address: {
+          street: "Main Street 2",
+          city: "Springfield 2",
+          number: 1234,
+          zip: "12345",
+        },
+      });
+
+    expect(response2.status).toBe(200);
+
+    const listResponse = await request(app).get("/customer").send();
+    expect(listResponse.status).toBe(200);
+    expect(listResponse.body.customers.length).toBe(2);
+    const customer = listResponse.body.customers[0];
+    expect(customer.name).toBe("John Doe");
+    expect(customer.address.street).toBe("Main Street");
+    const customer2 = listResponse.body.customers[1];
+    expect(customer2.name).toBe("Jane Doe");
+    expect(customer2.address.street).toBe("Main Street 2");
+  });
+});
